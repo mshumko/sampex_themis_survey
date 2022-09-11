322712ca060f66a36a18d11e199f0f6e23c8b75f
Mykhaylo Shumko <msshumko@gmail.com>
2022-09-11 09:31:57 -0400

    Cleaned up how the subplots are generated.

--- sampex_themis_survey/triple/summary_plots.py
+++ sampex_themis_survey/triple/summary_plots.py
@@ -86,35 +86,53 @@ class Themis_Themis_ASI:
         return
 
     def plot(self, save=True):
-        # TODO: Create the subplots here and pass each subplot to the relevant method.
-        self.fig = plt.figure(figsize=(12, 9))
-        self.spec = gridspec.GridSpec(
-            nrows=5, 
-            ncols=self.n_images, figure=self.fig, height_ratios=(2, 1, 1, 1, 1)
-            )
+        """
+        Creates the subplot layout and dispatches the plotting to the other methods.
+        """
+        self._create_empty_subplots()
+
         # -1 so that image_time[-1] == time_range[1]
         dt = (self.time_range[1]-self.time_range[0])/(self.n_images-1)
         self.image_times = [self.time_range[0] +  i*dt for i in range(self.n_images)]
-        self._plot_asi_images()
+        self._plot_asi_images(self.ax)
         try:
-            self._plot_themis_footprint()
+            self._plot_themis_footprint(self.ax)
         except AssertionError as err:
             if 'The time stamp difference' in str(err):
                 return
             else:
                 raise
-        self._plot_keogram()
+        self._plot_keogram(self.bx)
+
+        # The try-except blocks in case one of the instruments did not have data
+        # on that day.
         try:
-            self._plot_sst_e()
-            self._plot_sst_p()
-            self._plot_fbk()
+            self._plot_sst_e(self.cx)
         except FileNotFoundError as err:
             if 'does not contain any hyper references containing' in str(err):
                 return
             else:
                 raise
-        # self.spec.tight_layout(self.fig)
-        # plt.show()
+        try:
+            self._plot_sst_p(self.dx)
+        except FileNotFoundError as err:
+            if 'does not contain any hyper references containing' in str(err):
+                return
+            else:
+                raise
+        try:
+            self._plot_fbk(self.ex)
+        except FileNotFoundError as err:
+            if 'does not contain any hyper references containing' in str(err):
+                return
+            else:
+                raise
+
+        self.ex.set_xlabel('Time [HH:MM]')
+        time_format = matplotlib.dates.DateFormatter('%H:%M')
+        self.ex.xaxis.set_major_formatter(time_format)
+
+        # Annotate and clean up the plot.
         plot_labels = (
             'THEMIS-SST electrons',
             'THEMIS-SST ions',
@@ -138,13 +156,43 @@ class Themis_Themis_ASI:
             plt.show()
         return
 
-    def _plot_asi_images(self):
+    def _create_empty_subplots(self):
+        """
+        Create the empty subplot layout and make the subplots
+        as class attributes. 
+        """
+        self.fig = plt.figure(figsize=(12, 9))
+        self.spec = gridspec.GridSpec(
+            nrows=5, 
+            ncols=self.n_images, figure=self.fig, height_ratios=(2, 1, 1, 1, 1)
+            )
+        # ASI map subplots
+        self.ax = self.n_images*[None]
+        for i in range(self.n_images):
+            self.ax[i] = self.fig.add_subplot(self.spec[0, i])
+            self.ax[i].get_xaxis().set_visible(False)
+            self.ax[i].get_yaxis().set_visible(False)
+        # Keogram
+        self.bx = self.fig.add_subplot(self.spec[1, :])
+        self.bx.tick_params(axis="x", labelbottom=False)
+        # THEMIS-SST electrons
+        self.cx = self.fig.add_subplot(self.spec[2, :], sharex=self.bx) 
+        self.cx.tick_params(axis="x", labelbottom=False)
+        # THEMIS-SST protons
+        self.dx = self.fig.add_subplot(self.spec[3, :], sharex=self.bx) 
+        self.dx.tick_params(axis="x", labelbottom=False) 
+        # THEMIS-FBK magnetic spectrum. No need for the tick_params
+        # command since it is the bottom subplot.
+        self.ex = self.fig.add_subplot(self.spec[4, :], sharex=self.bx)
+        return
+
+
+    def _plot_asi_images(self, _ax):
         """
         Plot a sequence of ASI images projected onto a geographic map in the first row
         of the subplot grid.
         """
-        self.ax = self.n_images*[None]
-        z = zip(self.image_times, string.ascii_uppercase[:self.n_images])
+        z = zip(_ax, self.image_times, string.ascii_uppercase[:self.n_images])
 
         skymap = asilib.load_skymap('THEMIS', self.asi_location, self.image_times[0])
         self.lat_bounds = (
@@ -156,18 +204,15 @@ class Themis_Themis_ASI:
             skymap['SITE_MAP_LONGITUDE']+12
             )
 
-        for i, (image_time, subplot_letter) in enumerate(z):
-            self.ax[i] = self.fig.add_subplot(self.spec[0, i])
-            self.ax[i].get_xaxis().set_visible(False)
-            self.ax[i].get_yaxis().set_visible(False)
+        for i, (ax_i, image_time, subplot_letter) in enumerate(z):
             asilib.make_map(lat_bounds=self.lat_bounds, lon_bounds=self.lon_bounds,
-                ax=self.ax[i], land_color='grey')
+                ax=ax_i, land_color='grey')
 
             try:
                 # Update image_times with the actual image timestamp.
                 self.image_times[i], _, _, _, _ = asilib.plot_map(
                     'THEMIS', self.asi_location, image_time, self.map_alt, 
-                    ax=self.ax[i], asi_label=False, color_bounds=None, pcolormesh_kwargs={'rasterized':True},
+                    ax=ax_i, asi_label=False, color_bounds=None, pcolormesh_kwargs={'rasterized':True},
                     time_thresh_s=3
                     )
             except AssertionError as err:
@@ -176,18 +221,18 @@ class Themis_Themis_ASI:
                 else:
                     raise
             
-            self.ax[i].text(0, 0, f'({subplot_letter}) {image_time.strftime("%H:%M:%S")}', 
-                transform=self.ax[i].transAxes, va='bottom', color='k', fontsize=15)
+            ax_i.text(0, 0, f'({subplot_letter}) {image_time.strftime("%H:%M:%S")}', 
+                transform=ax_i.transAxes, va='bottom', color='k', fontsize=15)
         return
 
-    def _plot_themis_footprint(self):
+    def _plot_themis_footprint(self, _ax):
         """
         Plot the THEMIS probe footprint location in the first row of the subplot grid.
         """
         f = THEMIS_footprint(self.sc_id, self.time_range)
         f.map(alt=self.map_alt, model='T89', mag_input={'Kp':1.667})
         
-        for time, ax_i in zip(self.image_times, self.ax):
+        for time, ax_i in zip(self.image_times, _ax):
             dt = np.abs([(time-ti).total_seconds() for ti in f.time])
             idt = np.argmin(dt)
             assert (time-f.time[idt]).total_seconds() <= 60, (
@@ -198,73 +243,64 @@ class Themis_Themis_ASI:
                 color='r', s=100, marker='x')
         return
 
-    def _plot_keogram(self):
+    def _plot_keogram(self,_ax):
         """
         Plots a keogram on the 2nd row of the subplot grid.
         """
-        self.bx = self.fig.add_subplot(self.spec[1, :])
-        self.bx.tick_params(axis="x", labelbottom=False)
         try:
             asilib.plot_keogram('THEMIS', self.asi_location, self.time_range, 
-                ax=self.bx, map_alt=self.map_alt, aacgm=True)
+                ax=_ax, map_alt=self.map_alt, aacgm=True)
         except ValueError as err:
             if 'time stamps are out of order' in str(err):
                 print(err)
                 return
             else:
                 raise
-        self.bx.set_xlim(self.time_range)
-        self.bx.set_title('')
-        self.bx.text(0, 1, f'({string.ascii_uppercase[self.n_images+1]}) THEMIS ASI keogram', 
-            transform=self.bx.transAxes, va='top', color='white', fontsize=15)
-        self.bx.set_ylabel('Magnetic lat [deg]')
+        _ax.set_xlim(self.time_range)
+        _ax.set_title('')
+        _ax.text(0, 1, f'({string.ascii_uppercase[self.n_images+1]}) THEMIS ASI keogram', 
+            transform=_ax.transAxes, va='top', color='white', fontsize=15)
+        _ax.set_ylabel('Magnetic lat [deg]')
         # self.bx.set_ylim(self.lat_bounds)
         return
 
-    def _plot_sst_e(self):
+    def _plot_sst_e(self, _ax):
         """ 
         Plot the THEMIS-SST electrons.
-        """
-        self.cx = self.fig.add_subplot(self.spec[2, :], sharex=self.bx) 
-        self.cx.tick_params(axis="x", labelbottom=False) 
-
+        """ 
         # Electrons
-        cx_c = self.cx.inset_axes([1.01, 0, 0.02, 1], transform=self.cx.transAxes)
+        ax_c = _ax.inset_axes([1.01, 0, 0.02, 1], transform=_ax.transAxes)
         s = SST(self.sc_id, self.time_range[0], species='e')
         s.load()
-        _, p = s.spectrum(ax=self.cx, 
+        _, p = s.spectrum(ax=_ax, 
             pcolormesh_kwargs={'norm':matplotlib.colors.LogNorm(vmin=1E2, vmax=5E5)}
             )
-        plt.colorbar(p, ax=self.cx, cax=cx_c, label='Electron flux')
-        self.cx.set_xlim(self.time_range)
-        self.cx.set_ylabel('Energy [keV]')
-        self.cx.set_yscale('log')
+        plt.colorbar(p, ax=_ax, cax=ax_c, label='Electron flux')
+        _ax.set_xlim(self.time_range)
+        _ax.set_ylabel('Energy [keV]')
+        _ax.set_yscale('log')
         return
 
-    def _plot_sst_p(self):
+    def _plot_sst_p(self, _ax):
         """ 
         Plot the THEMIS-SST protons.
         """
-        self.dx = self.fig.add_subplot(self.spec[3, :], sharex=self.bx) 
-        self.dx.tick_params(axis="x", labelbottom=False) 
-        dx_c = self.dx.inset_axes([1.01, 0, 0.02, 1], transform=self.dx.transAxes)
+        ax_c = _ax.inset_axes([1.01, 0, 0.02, 1], transform=_ax.transAxes)
         s = SST(self.sc_id, self.time_range[0], species='i')
         s.load()
-        _, p = s.spectrum(ax=self.dx, 
+        _, p = s.spectrum(ax=_ax, 
             pcolormesh_kwargs={'norm':matplotlib.colors.LogNorm(vmin=0.1, vmax=1E6)}
             )
-        plt.colorbar(p, cax=dx_c, label='ion flux')
-        self.dx.set_ylabel('Energy [keV]')
-        self.dx.set_yscale('log')
+        plt.colorbar(p, cax=ax_c, label='ion flux')
+        _ax.set_ylabel('Energy [keV]')
+        _ax.set_yscale('log')
         return
 
-    def _plot_fbk(self):
+    def _plot_fbk(self, _ax):
         """
         Plot the THEMIS-FBK wave data
         """
-        self.ex = self.fig.add_subplot(self.spec[4, :], sharex=self.bx) 
-        # self.ex.tick_params(axis="x", labelbottom=False) 
-        ex_c = self.ex.inset_axes([1.01, 0, 0.02, 1], transform=self.ex.transAxes)
+        ax_c = _ax.inset_axes([1.01, 0, 0.02, 1], transform=_ax.transAxes)
         s = FBK(self.sc_id, self.time_range[0])
         try:
             s.load()
@@ -273,19 +309,16 @@ class Themis_Themis_ASI:
                 return
             else:
                 raise
-        _, p = s.spectrum(ax=self.ex, 
+        _, p = s.spectrum(ax=_ax, 
             pcolormesh_kwargs={'norm':matplotlib.colors.LogNorm(vmin=1E-4, vmax=0.1)})
-        plt.colorbar(p, cax=ex_c, label=f'$nT^{{{2}}}/Hz$')
-        self.ex.set_xlabel('Time [HH:MM]')
-        self.ex.set_ylabel('Frequency [Hz]') 
-        self.ex.set_yscale('log')
-        self.ex.set_ylim(1, 1E3)
-        time_format = matplotlib.dates.DateFormatter('%H:%M')
-        self.ex.xaxis.set_major_formatter(time_format)
+        plt.colorbar(p, cax=ax_c, label=f'$nT^{{{2}}}/Hz$')
+        _ax.set_ylabel('Frequency [Hz]') 
+        _ax.set_yscale('log')
+        _ax.set_ylim(1, 1E3)
         return
 
 
-class Sampex_Themis_ASI(Themmis_Themis_ASI):
+class Sampex_Themis_ASI(Themis_Themis_ASI):
     def __init__(self, c_filename, time_window_sec=3600/2, n_images=3, map_alt=110) -> None:
         super().__init__(c_filename, time_window_sec=time_window_sec, n_images=n_images, map_alt=map_alt)
         return
