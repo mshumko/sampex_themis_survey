fc2ac46e1378a1ade98aebfcd47a6a157b255702
Mykhaylo Shumko <msshumko@gmail.com>
2022-12-06 15:18:11 -0500

    Debugging the plot code.

--- sampex_themis_survey/__init__.py
+++ sampex_themis_survey/__init__.py
@@ -9,7 +9,7 @@ here = pathlib.Path(__file__).parent.resolve()
 settings = configparser.ConfigParser()
 settings.read(here / 'config.ini')
 
-config = {'code_dir': here}
+config = {'code_dir': here, 'data_dir': here / 'data'}
 
 # # Go here if config.ini exists (don't crash if the project is not yet configured.)
 # if 'Paths' in settings:  
